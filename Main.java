7a93361187f0cd397855be584bfb1e33d3072c1e
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2022-02-06 19:45:56 -0500

    Solved sort colours question.

--- Main.java
+++ Main.java
@@ -1,6 +1,29 @@
 import java.util.*;
 
 public class Main{
+    
+    public static int[] sortColors(int[] colors){
+        int start = 0;
+        int end = colors.length -1;
+        int index = 0;
+
+        while(index <= end && start < end){
+            if(colors[index] == 0){
+                colors[index] = colors[start];
+                colors[start] = 0;
+                index++;
+                start++;
+            }else if(colors[index] == 2){
+                colors[index] = colors[end];
+                colors[end] = 2;
+                end--;
+            }else{
+                index ++;
+            }
+        }
+
+        return colors;
+    }
 
     public static void main(String[] args){
         System.out.println("Lets get this started!");
