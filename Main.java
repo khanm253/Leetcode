933ce88a0ffb41d04b6115d07e526b758325b9b6
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2022-02-18 21:31:41 -0500

    Product except self and maxsubarray

--- Main.java
+++ Main.java
@@ -33,12 +33,75 @@ public class Main{
         return maxProfit;
     }
 
+    public static boolean containsDuplicate(int[] nums){ //O(N) time and space
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums){
+            if(set.contains(i)){
+                return true;
+            }
+            else{
+                set.add(i);
+            }
+        }
+        return false;
+    }
+
+    public static int[] productExceptSelf(int[] nums){ //O(N) time O(1) space
+        int[] result = new int[nums.length];
+        int rp = 1;
+
+        for(int i = 0; i < nums.length; i++){
+            result[i] = rp;
+            rp = rp * result[i];
+        }
+
+        rp = 1;
+
+        for(int i = nums.length - 1; i >= 0; i--){
+            result[i] = rp * result[i];
+            rp = rp * result[i];
+        }
+
+        return result;
+    }
+
+    public static int maxSubarrayQuadratic(int[] nums){ //O(N) and O(1)
+        int currSum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i; j< nums.length; j++){
+                currSum += nums[j];
+                max = Math.max(currSum, max);
+            }
+            currSum = 0;
+        }
+        return max;
+    }
+
+    //Kadane's algorithm
+    public static int maxSubArray(int[] nums){
+        int prefix = 0;
+        int max = nums[0];
+
+        for(int i = 0; i < nums.length; i++){
+            if(prefix < 0){
+                prefix = 0;
+            }
+            prefix += nums[i];
+            max = Math.max(max, prefix);
+            
+        }
+
+        return max;
+    }
+
 
     public static void main(String[] args){
         System.out.println("Lets get this started!");
-        int[] arr = new int[]{1,3,4,5,2,7};
+        int[] arr = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
 
-        System.out.println(buyAndSellStocks(arr)); 
+        System.out.println(maxSubArray(arr)); 
     }
 
 }
