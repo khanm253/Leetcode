dc84965d7b9a789b583767886d00e7b30db507ce
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2022-03-04 00:26:49 -0500

    starting dp

--- Main.java
+++ Main.java
@@ -346,34 +346,62 @@ public class Main{
     }
 
     public static int coinChange(int[] coins, int amount) {
-        int min = Integer.MAX_VALUE;
-        
-        int n = coins.length;
-        int m = amount;
-        int[][] dp = new int[n+1][m+1];
+        int[] dp = new int[amount + 1]; 
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
 
-        for(int i = 0; i < n+1; i++){
-            for(int j = 0; j < m+1; j++){
-                if(i==0 || j == 0){
+        for(int i = 1; i < amount + 1; i++){
+            for(int j = 0; j < coins.length; j++){
+                if(coins[j] <= i){
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                }else{
+                    break;
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public static int maxWaysCoin(int[] coins,  int amount){
+        int m = coins.length;
+        int n = amount;
+
+        int[][] dp = new int[n+1][m+1];
+ 
+        for(int i = 0; i < n + 1; i++){
+            for(int j = 0; j < m + 1 ; j++){
+                if(i == 0){
+                    dp[i][j] = 1;
+                }else if(j == 0){
                     dp[i][j] = 0;
                 }
             }
         }
 
-        for(int i = 1; i < n+1; i++){
-            for(int j = 1; j < m+1; j++){
-                dp[i][j] = Math.min(dp[i][j], 1 + dp[i][j - coins[i]]);
-            }
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < m + 1 ; j++){
+                if(coins[j-1] <= i){
+                    dp[i][j] = dp[i][j-1] + dp[i - coins[j-1]][j];
+                }else{
+                    dp[i][j] = dp[i][j-1];
+                }
+             }
         }
 
+        for(int i = 0; i < n + 1; i++){
+            for(int j = 0; j < m + 1 ; j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println("");
+        }
         return dp[n][m];
     }
 
     public static void main(String[] args){
         System.out.println("Lets get this started!");
-        int[] arr = new int[]{0,1,3};
+        int[] arr = new int[]{1,2,3};
 
-        System.out.println(climbStairs(2)); 
+        System.out.println(maxWaysCoin(arr, 5)); 
     }
 
 }
